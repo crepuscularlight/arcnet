3428d44f9ec3addda1b9260f89183a70ff97d7d6
Fidel Esquivel <fidel.esquivelestay@gmail.com>
2020-11-17 19:46:08 +0100

    working trainer with 60 classes

--- taco_train.py
+++ taco_train.py
@@ -92,7 +92,7 @@ if args.command == "train":
 elif args.command == 'test':
     # Inference should use the config with parameters that are used in training
     cfg.MODEL.WEIGHTS = os.path.join(cfg.OUTPUT_DIR, "model_final.pth")  # path to the model we just trained
-    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.2  # set a custom testing threshold
+    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5  # set a custom testing threshold
     predictor = DefaultPredictor(cfg)
 
     for d in random.sample(dataset_dicts_val, 10):
