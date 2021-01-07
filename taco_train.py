d41db89b661ec9fc574a4bdca8bb23276a8c1730
Fidel Esquivel <fidel.esquivelestay@gmail.com>
2021-01-07 18:21:18 +0100

    working ARCNet for experiment and testing

--- taco_train.py
+++ taco_train.py
@@ -197,7 +197,7 @@ cfg.TEST.EVAL_PERIOD = 50
 cfg.DATALOADER.NUM_WORKERS = 2
 cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")  # Let training initialize from model zoo
 cfg.SOLVER.IMS_PER_BATCH = 4
-cfg.SOLVER.BASE_LR = 0.01  # Starting lr. Adaptive.
+cfg.SOLVER.BASE_LR = 0.0025  # Starting lr scheduling.
 cfg.SOLVER.MAX_ITER = 1500
 cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 512 # (default: 512)
 cfg.MODEL.ROI_HEADS.NUM_CLASSES = args.class_num  # (see https://detectron2.readthedocs.io/tutorials/datasets.html#update-the-config-for-new-datasets)
@@ -206,7 +206,7 @@ os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
 # Freeze the first several stages so they are not trained.
 # There are 5 stages in ResNet. The first is a convolution, and the following
 # stages are each group of residual blocks.
-cfg.MODEL.BACKBONE.FREEZE_AT = 2 # default is 2
+cfg.MODEL.BACKBONE.FREEZE_AT = 1 # default is 2
 
 # Getting mAP calculation from Train loop
 train_continue = "FALSE" # Make this a passable argument #TODO
